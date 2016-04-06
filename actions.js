0e5d8807cab7a0ee4e433de1d09af2e80621caad
Oana Rosca <oanarosca1999@yahoo.com>
2016-04-06 23:42:15 +0300

    Added brief photo descriptions

--- actions.js
+++ actions.js
@@ -1,1 +1,12 @@
+var a = ["Me and my friends really like The Beatles.",
+"One of the prettiest libraries, Cluj-Napoca.",
+"We've been awarded by the County Council for the olympiads!",
+"Cluj-Napoca, as seen from a hotel's 8th floor.",
+"August 15th - National Navy Day!"];
 
+$(".pics li").hover(function() {
+  var id = $(this).attr("id");
+  document.getElementById("display").innerHTML = a[id];
+}, function() {
+  document.getElementById("display").innerHTML = "";
+});
